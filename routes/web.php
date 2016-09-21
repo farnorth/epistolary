e5fbf41594895860d93de6b51e38714bd78d8f81
ryanwinchester <fungku@gmail.com>
2016-09-21 10:08:16 -0700

    Comment out routes without controllers for now

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,8 @@ $router->get('newsletters/dashboard', [
 // Newsletters and sub-resources
 $router->resource('newsletters', 'NewslettersController');
 $router->resource('newsletters.campaigns', 'CampaignsController');
-$router->resource('newsletters.subscriptions', 'NewsletterSubscriptionsController');
+// $router->resource('newsletters.subscriptions', 'NewsletterSubscriptionsController');
 
 // Subscribers
-$router->resource('newsletters/subscribers', 'SubscribersController');
-$router->resource('newsletters/subscribers.subscriptions', 'SubscriberSubscriptionsController');
+// $router->resource('newsletters/subscribers', 'SubscribersController');
+// $router->resource('newsletters/subscribers.subscriptions', 'SubscriberSubscriptionsController');
