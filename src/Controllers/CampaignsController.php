a101d3c5fa393f9a800d3574047625abffb717f7
ryanwinchester <fungku@gmail.com>
2016-09-21 10:04:49 -0700

    Add campaigns controller

---
+++ src/Controllers/CampaignsController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Pilaster\Newsletters\Controllers;
+
+use Illuminate\Http\Request;
+use Pilaster\Newsletters\Campaign;
+
+class CampaignsController extends Controller
+{
+    /**
+     * Display a listing of newsletters.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function index()
+    {
+        $campaigns = Campaign::all();
+
+        return view('newsletters::campaigns.index', compact('campaigns'));
+    }
+
+    /**
+     * Show a single newsletter.
+     *
+     * @param int|string $campaign_id
+     * @return \Illuminate\View\View
+     */
+    public function show($campaign_id)
+    {
+        $campaign = $this->getCampaign($campaign_id);
+
+        return view('newsletters::campaigns.show', compact('campaign'));
+    }
+
+    /**
+     * Show the form to edit a newsletter.
+     *
+     * @param int|string $campaign_id
+     * @return \Illuminate\View\View
+     */
+    public function edit($campaign_id)
+    {
+        $campaign = $this->getCampaign($campaign_id);
+
+        return view('newsletters::campaigns.edit', compact('campaign'));
+    }
+
+    /**
+     * Update a newsletter.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int|string $campaign_id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(Request $request, $campaign_id)
+    {
+        $campaign = $this->getCampaign($campaign_id);
+        $campaign->update($this->newsletterAttributesFrom($request));
+
+        session()->flash('success', sprintf('Updated newsletter %s', $campaign->name));
+
+        return redirect()->route('newsletters.index');
+    }
+
+    /**
+     * Show the form to create a new newsletter.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('newsletters::campaigns.create');
+    }
+
+    /**
+     * Save a newly created newsletter to the database.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $campaign = new Campaign($this->newsletterAttributesFrom($request));
+        $campaign->save();
+
+        session()->flash('success', sprintf('Created newsletter %s', $campaign->name));
+
+        return redirect()->route('newsletters.index');
+    }
+
+    /**
+     * Delete a newsletter.
+     *
+     * @param int|string $campaign_id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy($campaign_id)
+    {
+        $campaign = $this->getCampaign($campaign_id);
+        $campaign->delete();
+
+        session()->flash('success', sprintf('Deleted newsletter %s', $campaign->name));
+
+        return redirect()->route('newsletters.index');
+    }
+
+    /**
+     * Get a newsletter by its ID or slug.
+     *
+     * @param int|string $campaign
+     * @return \Pilaster\Newsletters\Campaign
+     */
+    private function getCampaign($campaign)
+    {
+        if (is_numeric($campaign)) {
+            return Campaign::find($campaign);
+        }
+
+        return Campaign::getBySlug($campaign);
+    }
+
+    /**
+     * Build the array of attributes for a newsletter from a request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    private function newsletterAttributesFrom(Request $request)
+    {
+        return [
+            'name' => $request->input('name'),
+            'slug' => str_slug($request->input('name')),
+            'description' => $request->input('description'),
+            'requires_opt_in' => $request->input('requires_opt_in', true),
+        ];
+    }
+}
---
+++ views/campaigns/show.blade.php
@@ -0,0 +1,6 @@
+@extends('newsletters::layout')
+
+@section('content')
+  <h1>{{ $campaign->name }}</h1>
+  <h2>Campaign</h2>
+@endsection
