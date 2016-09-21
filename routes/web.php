85409d1973b1cd772aca539ec862be93ec1e1530
ryanwinchester <fungku@gmail.com>
2016-09-21 10:58:19 -0700

    Guess we need to go the explicit routing...route

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,25 @@ $router->get('newsletters/dashboard', [
     'as' => 'dashboard',
 ]);
 
-$router->resource('newsletters/lists', 'ListsController');
+
+$router->get('newsletters/lists', [
+    'uses' => 'ListsController@index',
+    'as' => 'lists.index',
+]);
+$router->get('newsletters/lists/{list_id}', [
+    'uses' => 'ListsController@show',
+    'as' => 'lists.show',
+]);
+
+
 // $router->resource('newsletters/lists.subscriptions', 'ListSubscriptionsController');
-$router->resource('newsletters/campaigns', 'CampaignsController');
+$router->get('newsletters/campaigns', [
+    'uses' => 'CampaignsController@index',
+    'as' => 'campaigns.index'
+]);
+$router->get('newsletters/campaigns/{campaign_id}', [
+    'uses' => 'CampaignsController@show',
+    'as' => 'campaigns.show'
+]);
 // $router->resource('newsletters/subscribers', 'SubscribersController');
 // $router->resource('newsletters/subscribers.subscriptions', 'SubscriberSubscriptionsController');
